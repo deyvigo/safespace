ef393392692e6f306560fcc5419fbd13a3888c38
deyvigo <deyvigo23@gmail.com>
2025-10-21 11:34:22 -0500

    first commit

---
+++ safespace-back/src/main/java/com/example/safespace_back/SafespaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.safespace_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafespaceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafespaceBackApplication.class, args);
+	}
+
+}
---
+++ safespace-back/src/test/java/com/example/safespace_back/SafespaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safespace_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafespaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
