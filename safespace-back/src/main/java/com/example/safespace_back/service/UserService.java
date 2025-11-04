a2ea2befe1772f3706146af2cd776c214a409fcc
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-11-04 01:23:23 -0500

    feat: add user service

---
+++ safespace-back/src/main/java/com/example/safespace_back/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.safespace_back.service;
+
+import com.example.safespace_back.dto.in.UpdatePsychologistRequestDTO;
+import com.example.safespace_back.dto.in.UpdateStudentRequestDTO;
+import com.example.safespace_back.dto.out.PsychologistDTO;
+import com.example.safespace_back.dto.out.StudentDTO;
+import com.example.safespace_back.model.UserEntity;
+
+public interface UserService {
+  Object findByUsername(String username, UserEntity currentUser);
+  StudentDTO updateStudent(UpdateStudentRequestDTO updateDTO, UserEntity currentUser);
+  PsychologistDTO updatePsychologist(UpdatePsychologistRequestDTO updateDTO, UserEntity currentUser);
+}
