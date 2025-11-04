6d9834461b6f5fb691096ba0e4b6feec4fc28aa1
deyvigo <deyvigo23@gmail.com>
2025-11-04 00:10:47 -0500

    fix: validation for register student and psychologist

--- safespace-back/src/main/java/com/example/safespace_back/dto/in/LoginUserDTO.java
+++ safespace-back/src/main/java/com/example/safespace_back/dto/in/LoginUserDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record LoginUserDTO (
     @NotBlank(message = "username is required")
+    @Size(min = 6, message = "username must have more than 6 characters")
     String username,
     @NotBlank(message = "password is required")
     @Size(min = 6, max = 18, message = "password must be between 6 and 18 characters long")
--- safespace-back/src/main/java/com/example/safespace_back/dto/in/RegisterPsychologistRequestDTO.java
+++ safespace-back/src/main/java/com/example/safespace_back/dto/in/RegisterPsychologistRequestDTO.java
@@ -3,13 +3,16 @@ package com.example.safespace_back.dto.in;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record RegisterPsychologistRequestDTO (
     @NotBlank(message = "username is required")
+    @Size(min = 6, message = "username must have more than 6 characters")
     String username,
     @NotBlank(message = "password is required")
+    @Size(min = 6, max = 18, message = "password must be between 6 and 18 characters long")
     String password,
     @NotBlank(message = "name is required")
     String name,
