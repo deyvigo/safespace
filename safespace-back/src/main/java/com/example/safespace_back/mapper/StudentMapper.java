2523805c900afdc59f7be4e0c74b8d1d265104e4
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-11-04 01:22:37 -0500

    feat: add new method in student mapper

--- safespace-back/src/main/java/com/example/safespace_back/mapper/StudentMapper.java
+++ safespace-back/src/main/java/com/example/safespace_back/mapper/StudentMapper.java
@@ -2,6 +2,7 @@ package com.example.safespace_back.mapper;
 
 import com.example.safespace_back.dto.in.RegisterStudentRequestDTO;
 import com.example.safespace_back.dto.out.RegisterStudentResponseDTO;
+import com.example.safespace_back.dto.out.StudentDTO;
 import com.example.safespace_back.model.StudentEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -10,4 +11,6 @@ import org.mapstruct.MappingConstants;
 public interface StudentMapper {
     StudentEntity toEntity(RegisterStudentRequestDTO dto);
     RegisterStudentResponseDTO toResponse(StudentEntity userEntity);
+    
+    StudentDTO toStudentDTO(StudentEntity studentEntity);
 }
