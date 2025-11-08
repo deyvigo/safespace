5fd44653c2887526e1ce7f52e4f85a468b86985d
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-11-08 11:30:08 -0500

    feat: add digital resource repository

---
+++ safespace-back/src/main/java/com/example/safespace_back/repository/DigitalResourcesRepository.java
@@ -0,0 +1,10 @@
+package com.example.safespace_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.safespace_back.model.DigitalResourcesEntity;
+
+public interface DigitalResourcesRepository extends JpaRepository<DigitalResourcesEntity, Long> {
+
+  
+}
