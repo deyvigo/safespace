4801b4f34c6e9fe9114b0e926047ec93fc1b08f8
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-11-23 12:30:13 -0500

    feat: remove the unnecessary decorator component

--- safespace-back/src/main/java/com/example/safespace_back/mapper/StudentMapper.java
+++ safespace-back/src/main/java/com/example/safespace_back/mapper/StudentMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Component;
 
-@Component
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface StudentMapper {
     StudentEntity toEntity(RegisterStudentRequestDTO dto);
